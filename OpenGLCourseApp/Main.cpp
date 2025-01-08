d719e79e7c4cdeda44abe223e2dda238305ee20b
Matt Prangley <mprangley3d@gmail.com>
2025-01-08 14:56:34 -0500

    Fixed linkers/libs after project move/rename

--- OpenGLCourseApp/Main.cpp
+++ OpenGLCourseApp/Main.cpp
@@ -8,6 +8,20 @@ const GLint WIDTH = 800, HEIGHT = 600;
 
 int main()
 {
+    //Initialize GLFW
+    if (!glfwInit())
+    {
+        printf("GLFW Initialization failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup GLFW Window Properties
+    // Open GL Version (3.3)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 
     return 0;
 }
