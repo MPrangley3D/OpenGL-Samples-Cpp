e5ae4776fd4d6e5d7fee148e32fcf6c6647a5d9c
Matt Prangley <mprangley3d@gmail.com>
2025-01-08 15:41:38 -0500

    Finished unit 3, window can be drawn on screen.

--- OpenGLCourseApp/Main.cpp
+++ OpenGLCourseApp/Main.cpp
@@ -2,13 +2,13 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-//Window dimensions
+// Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
 
 int main()
 {
-    //Initialize GLFW
+    // Initialize GLFW
     if (!glfwInit())
     {
         printf("GLFW Initialization failed!");
@@ -20,9 +20,56 @@ int main()
     // Open GL Version (3.3)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Core profile = No backwards compatability
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // Allow forward compatability
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // Create & ensure window exists
+    GLFWwindow* MainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+    if (!MainWindow)
+    {
+        printf("GLFW Window creation failed!");
+        glfwTerminate();
+        return 1;
+    }
 
+    // Get buffer size information
+    int BufferWidth;
+    int BufferHeight;
+    glfwGetFramebufferSize(MainWindow, &BufferWidth, &BufferHeight);
+
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(MainWindow);
+
+    // Allow modern extesion features
+    glewExperimental = GL_TRUE;
+
+    // Check if GLEW is working
+    if (glewInit() != GLEW_OK)
+    {
+        printf("GLEW Initialization failed!");
+        glfwDestroyWindow(MainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup viewport size
+    glViewport(0, 0, BufferWidth, BufferHeight);
+
+    // Loop until window closed
+    while (!glfwWindowShouldClose(MainWindow))
+    {
+        // Get + Handle User Input Events
+        glfwPollEvents();
+
+        // Clear window
+        glClearColor(1.0f,0.0f,0.0f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(MainWindow);
+    }
+
+    printf("User closed window.");
     return 0;
 }
 
