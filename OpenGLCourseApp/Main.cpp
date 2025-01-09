dd5baf9b0a67d9a21307f23901f440674c06df64
Matt Prangley <mprangley3d@gmail.com>
2025-01-09 14:32:16 -0500

    Spinning 3D triangle

--- OpenGLCourseApp/Main.cpp
+++ OpenGLCourseApp/Main.cpp
@@ -15,10 +15,13 @@ const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
 const float ToRadians = 3.14159265f / 180.0f;
 
-GLuint VAO;
-GLuint VBO;
+GLuint VAO;   // Vertex Array Object
+GLuint VBO;   // Vertex Buffer Object
+GLuint IBO;   // Index Buffer Object
 GLuint ShaderProgram;
 GLuint UniformModel;
+GLuint UniformView;
+GLuint UniformProjection;
 
 bool Direction = true;
 bool Growing = true;
@@ -54,67 +57,98 @@ Location 1: Color attribute (color, vertexColor, etc.)
 Location 2: Texture coordinates attribute (texCoord, uv, etc.)
 Location 3: Normal attribute (normal, vertexNormal, etc.)
 4-7 | Additional texture coordinates or other custom attributes
+ Modern cards support up to 16 locations
 */
 
 static const char* VertexShader = "                                     \n\
 #version 330                                                            \n\
                                                                         \n\
 layout (location = 0) in vec3 pos;                                      \n\
-                                                                        \n\
 out vec4 VertexColor;                                                   \n\
                                                                         \n\
-                                                                        \n\
 uniform mat4 Model;                                                     \n\
-                                                                        \n\
+uniform mat4 View;                                                      \n\
+uniform mat4 Projection;                                                \n\
                                                                         \n\
 void main()                                                             \n\
 {                                                                       \n\
-    gl_Position = Model * vec4(pos,1.0);                                \n\
+    gl_Position = Projection * Model * vec4(pos,1.0);                   \n\
     VertexColor = vec4(clamp(pos, 0.0f, 1.0f),1.0f);                    \n\
 }";
 
 // Fragment Shader
-static const char* FragmentShader = "                               \n\
-#version 330                                                        \n\
-                                                                    \n\
-in vec4 VertexColor;                                                \n\
-                                                                    \n\
-out vec4 color;                                                     \n\
-                                                                    \n\
-void main()                                                         \n\
-{                                                                   \n\
-    color = VertexColor;                                            \n\
+static const char* FragmentShader = "                                   \n\
+#version 330                                                            \n\
+                                                                        \n\
+in vec4 VertexColor;                                                    \n\
+                                                                        \n\
+out vec4 color;                                                         \n\
+                                                                        \n\
+void main()                                                             \n\
+{                                                                       \n\
+    color = VertexColor;                                                \n\
 }";
 
-void CreateTriangle()
+void CreateGeometry()
 {
-    // Hard code triangle vertex values
-    GLfloat TriangleVerticies[] = 
+    //Specify the point sets that make our pyramid
+    unsigned int Indicies[] =
     {
-        -1.0f, -1.0f, 0.0f,
-         1.0f, -1.0f, 0.0f,
-         0.0f,  1.0f, 0.0f
+        0,3,1,
+        1,3,2,
+        2,3,0,
+        0,1,2
     };
 
+    GLfloat GeometryVerticies[] = 
+    {
+          0.0f,  0.0f, 1.0f, //0
+         -1.0f, -1.0f, 0.0f, //1
+          1.0f, -1.0f, 0.0f, //2
+          0.0f,  1.0f, 0.0f  //3
+    };
+
+    /*  NOTES ON VERTEX SPECIFICATON
+    the critical OpenGL call in the code sample you give is actually glVertexAttribPointer:
+    When you call glVertexAttribPointer, what it does is interpret the parameters are relative to the currently bound buffer, 
+    and save off the state to the currently bound VAO. From this you can easily infer that it doesn’t actually matter what 
+    order the buffer and VAO are bound in; what matters is that they are bound when glVertexAttribPointer is called.
+
+    There is of course an exception: binding the GL_ELEMENT_ARRAY_BUFFER is actually different, and the current GL_ELEMENT_ARRAY_BUFFER 
+    is saved to the current VAO at the time it is bound. In that case, the VAO must be bound first.
+
+    For this reason, that VAO should be unbound BEFORE the IBO is unbound, to ensure no null references.
+    */
+
     // "VERTEX SPECIFICATION"
-// 1. Generate Vertex Array Object ID
+    // 1. Generate Vertex Array Object ID
     glGenVertexArrays(1, &VAO);
-// 2. Bind VAO to the ID
+    // 2. Bind VAO to the ID
     glBindVertexArray(VAO);
-        
-// 3. Generate VBO ID
-        glGenBuffers(1, &VBO);
-// 4. Bind VBO to ID
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-// 5. Attach vertex data to the bound VBO
-            glBufferData(GL_ARRAY_BUFFER, sizeof(TriangleVerticies), TriangleVerticies, GL_STATIC_DRAW);
-// 6. Define Attribute Pointer
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-// 7. Enable the Attribute Pointer
-            glEnableVertexAttribArray(0);
-// 8. Unbind the VAO and VBO
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-     glBindVertexArray(0);
+    // Create Index Buffer ID for verticies
+    glGenBuffers(1, &IBO);
+    // Bind index buffer io ID
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    // Attach vertex data to the bound IBO
+    // Due to the VAO, this IBO and the VBO below are automatically related
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indicies), Indicies, GL_STATIC_DRAW);
+    // 3. Generate VBO ID
+    glGenBuffers(1, &VBO);
+    // 4. Bind VBO to ID
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // 5. Attach vertex data to the bound VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GeometryVerticies), GeometryVerticies, GL_STATIC_DRAW);
+    // 6. Define Attribute Pointer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    // 7. Enable the Attribute Pointer
+    glEnableVertexAttribArray(0);
+    // 8. Unbind the VAO, VBO, and IBO
+    // IMPORTANT:  VAO should be unbound FIRST, then IBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Unbind IBO *AFTER* VBO 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void AddShader(GLuint TheProgram, const char* ShaderCode, GLenum ShaderType)
@@ -191,9 +225,14 @@ void CompileShaders()
         return;
     }
 
-    // Get the location of the MoveX Variable from the sahder code in the specified Shader Program
+    // Bind uniform model variable to the location of the model in the shader code
     UniformModel = glGetUniformLocation(ShaderProgram, "Model");
 
+    // Bind uniform view variable to the location of the model in the shader code
+    UniformView = glGetUniformLocation(ShaderProgram, "View");
+
+    // Bind uniform projection variable to the location of the model in the shader code
+    UniformProjection = glGetUniformLocation(ShaderProgram, "Projection");
 }
 
 int main()
@@ -244,11 +283,17 @@ int main()
         return 1;
     }
 
+    //Enable Depth Test
+    glEnable(GL_DEPTH_TEST);
+
     // Create viewport & setup size
     glViewport(0, 0, BufferWidth, BufferHeight);
 
-    CreateTriangle();
+    CreateGeometry();
     CompileShaders();
+
+    // We only need to set up Projection once, so we do it here rather than in the While loop
+    glm::mat4 Projection = glm::perspective(45.0f, (GLfloat)BufferWidth / (GLfloat)BufferHeight, 0.1f, 100.0f);
 
     // Loop until window closed
     while (!glfwWindowShouldClose(MainWindow))
@@ -302,7 +347,9 @@ int main()
 
         // Clear window
         glClearColor(0.0f,0.0f,0.0f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Clear the color & depth buffer bits
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Assign the Shader Program
         glUseProgram(ShaderProgram);
@@ -312,21 +359,31 @@ int main()
 
         // Update the Transform by the Model Matrix
         // Note these are applied in reserve order to the object
-        Model = glm::translate(Model, glm::vec3(TriangleOffset, TriangleOffset, 0.0f));
-        Model = glm::rotate(Model, CurrentAngle * ToRadians, glm::vec3(0.0f, 0.0f, 1.0f));    
-        Model = glm::scale(Model, glm::vec3(0.4f, 0.4f, 1.0f));
+        Model = glm::translate(Model, glm::vec3(0.0f, 0.0f, -2.5f));
+        Model = glm::rotate(Model, CurrentAngle * ToRadians, glm::vec3(0.5f, 0.5f, 0.5f)); 
+        Model = glm::rotate(Model, 270 * ToRadians, glm::vec3(1.0f, 0.0f, 0.0f));
+        Model = glm::scale(Model, glm::vec3(1.0f, 1.0f, 1.0f));
 
         // Bind the Uniform Model Matrix
         glUniformMatrix4fv(UniformModel, 1, GL_FALSE, glm::value_ptr(Model));
 
+        // Bind the Uniform Perspective / Projection Matrix
+        glUniformMatrix4fv(UniformProjection, 1, GL_FALSE, glm::value_ptr(Projection));
+
             // Bind the VAO
             glBindVertexArray(VAO);
 
-                // Draw the Shader
-                glDrawArrays(GL_TRIANGLES, 0, 3);
+            // Bind the Element Array Buffer
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+            // Draw the Elements
+            glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
             // Unbind the VAO
             glBindVertexArray(0);
+
+            // Unbind the Element Array Buffer
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
         // Clear the Shader Program
         glUseProgram(0);
